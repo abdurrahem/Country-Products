19a91449698711a72e1861c8f6acc2b80b83cfa8
abdurrahem <3bdalrahem.ah09@gmail.com>
2022-10-09 20:39:13 +0400

    add interceptor for authentication

--- app.module.ts
+++ app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthenicationComponent } from './authenication/authenication.component';
 import { AuthenicationService } from './authenication/authenication.service';
+import { AuthInterceptor } from './shared/interseptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { AuthenicationService } from './authenication/authenication.service';
     SharedModule,
     ReactiveFormsModule
   ],
-  providers: [AuthenicationService],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
